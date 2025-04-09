3df9ce6b667dda96f783461fecac4805093dcba9
creator566 <yks266715@gmail.com>
2025-04-09 22:43:42 +0530

    Update and rename run.sh to run.py

---
+++ run.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import subprocess,sys,os
+
+if(len(sys.argv)<2):
+    print("[ Error ] : Filename not given")
+    exit()
+
+name=str(sys.argv[1])
+
+outFolder="Output/"
+arguments=""
+
+for i in sys.argv:
+    if(i == "-debug"):
+        outFolder="Output/Debug/"
+    elif(i == "-release"):
+        outFolder="Output/Release/"
+    elif(i  == "-sanitizer"):
+        arguments=" -fsanitize=address,undefined,alignment"
+
+cppCommand="g++ -g -o "+outFolder+"output "+name+"  $(pkg-config --cflags --libs lua) -I"+os.getcwd()+" "+arguments+" && ./"+outFolder+"output"
+cCommand="gcc -g  -o "+outFolder+"output "+name+"  $(pkg-config --cflags --libs lua) -I"+os.getcwd()+" "+arguments+" && ./"+outFolder+"output"
+
+
+if(name.endswith('.cpp')):
+    os.system(cppCommand)
+elif(name.endswith('.c')):
+    os.system(cCommand)
+else:
+    print("[ Error ] : File is not C/C++")
+
