f8221c06f892df7e772b17b5059e2797f4046a15
subham008 <shaw.subham14.05.2002@gmail.com>
2025-04-14 21:47:20 +0530

    engine.cpp cretaed and module run queue created

--- Core/engine.cpp
+++ Core/engine.cpp
@@ -1,5 +1,26 @@
 #ifndef __ENGINE_CPP__
 #define __ENGINE_CPP__
 
-//TODO
+#include"componant_parser.cpp"
+#include<vector>
+
+class Engine{
+    private:
+       std::vector<Module> modulesQueue[2];
+       uint32_t current=0;
+
+   public:
+
+    Engine(){
+        modulelesQueue[0].reserve(15);
+        modulelesQueue[1].reserve(15);
+    }
+
+    void addModule(Module& m){
+        modulesQueue[current^1].push_back(m);
+        
+    }
+};
+
+
 #endif
