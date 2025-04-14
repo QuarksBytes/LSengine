7305d26fc3d5c113759c2166b410f32175757bbd
subham008 <shaw.subham14.05.2002@gmail.com>
2025-04-14 22:18:37 +0530

    engine.cpp updated , added desifn and Module registry in contsructer , componant are comverted to Modules through ModuleResgitry , and added those module in Engine class variavle modules , ModuleRegistry.cpp created anf getModule implememetd juts for test

--- Core/engine.cpp
+++ Core/engine.cpp
@@ -2,23 +2,33 @@
 #define __ENGINE_CPP__
 
 #include"componant_parser.cpp"
+#include"design_parser.cpp"
 #include<vector>
+
+
 
 class Engine{
     private:
-       std::vector<Module> modulesQueue[2];
+       std::vector<Module> modules;
+       std::vector<Module> runModulesQueue[2];
        uint32_t current=0;
 
    public:
 
-    Engine(){
-        modulelesQueue[0].reserve(15);
-        modulelesQueue[1].reserve(15);
+    Engine(Design& design , ModuleRegistry& moduleRegistry){
+       
+        //adding all modules from design
+        for(auto& component : design.components){
+            modules.push_back(moduleRegistry.getModule(component.identity)); 
+        }
+       
+        //reserving space for modulesQueue
+        runModulesQueue[0].reserve(15);
+        runModulesQueue[1].reserve(15);
     }
 
     void addModule(Module& m){
-        modulesQueue[current^1].push_back(m);
-        
+       modules.push_back(m);
     }
 };
 
---
+++ Core/module_registry.cpp
@@ -0,0 +1,19 @@
+#ifndef __MODULE_REGISTRY_CPP__
+#define __MODULE_REGISTRY_CPP__
+
+#include "componant_parser.cpp"
+#include <vector>
+#include <string>
+
+
+class ModuleRegistry {
+    public:
+      Module& getModule(const std::string& moduleName) {
+          //TODO
+          return NULL;
+      }
+
+};
+
+
+#endif
