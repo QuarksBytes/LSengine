e5e110dbda095889327f55ff1222b12c96d31486
subham008 <shaw.subham14.05.2002@gmail.com>
2025-04-15 23:48:23 +0530

    module registry implemented with getModule

--- Core/module_registry.cpp
+++ Core/module_registry.cpp
@@ -4,13 +4,40 @@
 #include "componant_parser.cpp"
 #include <vector>
 #include <string>
-
+#include <iostream>
+#include <map>
 
 class ModuleRegistry {
+    private:
+
+     std::map<std::string, int> moduleMap;
+     std::map<std::string, int> sourceMap;
+     
     public:
+     ModuleRegistry(std::vector<std::string> extensionSources){
+            sources = extensionSources;
+            for(auto source : extensionSources){
+                try{
+                 //converting to Module   
+                ComponantParser::json j;
+                source >> j;
+                ComponantParser::Module m = j.get<ComponantParser::Module>();
+                //adding to global vectors
+                modulesMap[m.module] = m;
+                //adding to source map
+                sourceMap[m.module] = source;
+                }
+                catch(const std::exception& e){
+                    std::cerr << "Error parsing JSON: " << e.what() << "\n";
+                    return 1;
+                }
+            }
+     }
+
+
       Module& getModule(const std::string& moduleName) {
           //TODO
-          return NULL;
+          return modulesMap[moduleName];
       }
 
 };
