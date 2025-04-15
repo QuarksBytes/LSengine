d0a3000c64e14006e488eb709b8f7a8200bd9da9
subham008 <shaw.subham14.05.2002@gmail.com>
2025-04-15 14:08:58 +0530

    engine updated : run function added

--- Core/engine.cpp
+++ Core/engine.cpp
@@ -25,10 +25,30 @@ class Engine{
         //reserving space for modulesQueue
         runModulesQueue[0].reserve(15);
         runModulesQueue[1].reserve(15);
+
+        //prepering first run queue , only componant eith type input is to be in run queue 1
+        for(auto& module : modules){
+            if(module.type == "input"){
+                runModulesQueue[0].push_back(module);
+            }
+        }
+
+
     }
 
     void addModule(Module& m){
        modules.push_back(m);
+    }
+
+
+    void run(){
+        //TODO
+        //run the modules in the current queue and add the output modules to the next queue
+        //after running all modules in the current queue, swap the queues and repeat until no more modules to run
+        for(auto& module : runModulesQueue[current]){
+            //run module
+        }
+        current = (current + 1) % 2;
     }
 };
 
