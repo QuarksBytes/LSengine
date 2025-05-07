51fcf1a1a3632f192370165a309dd5a3258f2afd
subham008 <shaw.subham14.05.2002@gmail.com>
2025-05-07 21:48:58 +0530

    design is pseodo implemented

--- Core/Design.cpp
+++ Core/Design.cpp
@@ -1,4 +1,16 @@
 #ifndef _DESIGN_CPP
 #define _DESIGN_CPP
 
+#include"Componant.cpp"
+#include<string>
+#include<vector>
+
+class Design
+{
+    public:
+    std::string name;
+    std::string description;
+    std::vector<Componant> components;
+    std::vector<Simulator> simulators;
+};
 #endif
